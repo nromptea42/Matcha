78e86b718f8adf392636fda6345f65ec6abe5d90
Nathan Rompteaux <nathan.rompteaux@gmail.com>
2016-10-27 18:31:31 +0200

    oui

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+var url = "mongodb://localhost:27017/test";
+var sha256 = require('js-sha256');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index');
+});
+
+router.post('/login', function(req, res, next) {
+    var email = req.body.email;
+    var mdp = req.body.mdp;
+    var my_cursor;
+
+    if (email && mdp) {
+        mongo.connect(url, function (err, db) {
+            assert.equal(null, err);
+            db.collection('user-data').findOne({email: email}).then(function (cursor) {
+                db.close();
+                if (cursor) {
+                    if(cursor.mdp == sha256(mdp + cursor.salt))
+                        res.redirect('liste/profil/' + cursor._id);
+                    else
+                        res.render('index');
+                }
+            });
+        });
+    }
 });
 
 module.exports = router;
